14534784a2b761b987976012e5a2d403b0d8e7fe
takayuki.inoue <takayuki.inoue@i-studio.co.jp>
2021-05-28 15:39:57 +0900

    #3 環境構築

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,19 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
 import {css} from "@emotion/react"
-
-import Layout from "../components/layout"
+import Layout from "../components/Organisms/layout"
 import Seo from "../components/seo"
 
+import astronaut from  "@/assets/images/gatsby-astronaut.png"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1 css={head}>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    <p className={"display-sp"}>Now go build something great.</p>
+    <div css={image} className={"display-pc"}>
+      <img src={astronaut} alt="hoge" />
+    </div>
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
@@ -30,9 +23,13 @@ const IndexPage = () => (
 
 const head = css({
   color: "#ff0000",
-  ["&:after"]: {
+  "&:after": {
     content: '""'
   }
+})
+
+const image = css({
+  width: "300px",
 })
 
 export default IndexPage
